8b53c5560fd7cdaaf77a000fccdda5b54ae1f77a
Cody Hodges <hodges.cody00@gmail.com>
2022-05-28 16:55:31 -0400

    Pikes Start

---
+++ src/main/java/com/codeup/pikes/PikesApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.pikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PikesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeup/pikes/PikesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.pikes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PikesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
