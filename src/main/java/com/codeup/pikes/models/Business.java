79c635eb2a76d2575192d8f1fe8e06f0de18d4d6
Cody Hodges <hodges.cody00@gmail.com>
2022-05-28 21:22:11 -0400

    Pikes Models Start

---
+++ src/main/java/com/codeup/pikes/models/Business.java
@@ -0,0 +1,38 @@
+package com.codeup.pikes.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "businesses")
+public class Business {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Business(){}
+
+    public Business(long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/codeup/pikes/models/User.java
@@ -0,0 +1,96 @@
+package com.codeup.pikes.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private boolean admin;
+
+    @Column(nullable = true)
+    private String profPic;
+
+
+    public User(){}
+
+    public User(User copy){
+        id = copy.id;
+        username = copy.username;
+        password = copy.password;
+        email = copy.email;
+        admin = copy.admin;
+        profPic = copy.profPic;
+    }
+
+    public User(long id, String username, String password, String email, boolean admin, String profPic){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.admin = admin;
+        this.profPic = profPic;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public String getProfPic() {
+        return profPic;
+    }
+
+    public void setProfPic(String profPic) {
+        this.profPic = profPic;
+    }
+}
