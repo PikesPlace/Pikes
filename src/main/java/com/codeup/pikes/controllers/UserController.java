523644b5aecf02d42eb49f8391088958d43b0953
Cody Hodges <hodges.cody00@gmail.com>
2022-05-29 22:47:46 -0400

    Security

--- src/main/java/com/codeup/pikes/controllers/UserController.java
+++ src/main/java/com/codeup/pikes/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.codeup.pikes.controllers;
 
 import com.codeup.pikes.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,7 @@ public class UserController {
 
     @GetMapping("/")
     public String index(Model model) {
-
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         return "index";
     }
 }
