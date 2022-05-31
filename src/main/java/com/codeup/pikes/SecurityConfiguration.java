9b070ced83c0437ff94a67fec1365e554cc23a3b
Cody Hodges <hodges.cody00@gmail.com>
2022-05-31 17:56:11 -0400

    Security password fix / view creations

--- src/main/java/com/codeup/pikes/SecurityConfiguration.java
+++ src/main/java/com/codeup/pikes/SecurityConfiguration.java
@@ -1,4 +1,67 @@
 package com.codeup.pikes;
 
-public class SecurityConfiguration {
+import com.codeup.pikes.services.UserDetailsLoader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsLoader usersLoader;
+
+    public SecurityConfiguration(UserDetailsLoader usersLoader) {
+        this.usersLoader = usersLoader;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(usersLoader) // How to find users by their username
+                .passwordEncoder(passwordEncoder()) // How to encode and verify passwords
+        ;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                /* Login configuration */
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/home") // user's home page
+                .permitAll() // Anyone can go
+
+
+                /* Logout configuration */
+                .and()
+                .logout()
+                .logoutSuccessUrl("/") // append a query string value
+
+
+                /* Pages that can be viewed without having to log in */
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/login", "/index") // anyone can see
+                .permitAll()
+
+
+                /* Pages that require authentication */
+                .and()
+                .authorizeRequests()
+                .antMatchers("/home")
+                .authenticated()
+
+
+        ;
+    }
 }
+
--- src/main/java/com/codeup/pikes/controllers/UserController.java
+++ src/main/java/com/codeup/pikes/controllers/UserController.java
@@ -1,32 +1,45 @@
 package com.codeup.pikes.controllers;
 
+import com.codeup.pikes.models.User;
 import com.codeup.pikes.repositories.BusinessRepository;
 import com.codeup.pikes.repositories.LocationRepository;
 import com.codeup.pikes.repositories.TicketRepository;
 import com.codeup.pikes.repositories.UserRepository;
-import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 public class UserController {
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userDao;
     private final BusinessRepository businessDao;
     private final LocationRepository locationDao;
     private final TicketRepository ticketDao;
 
-    public UserController(UserRepository userDao, BusinessRepository businessDao, LocationRepository locationDao, TicketRepository ticketDao) {
+    public UserController(UserRepository userDao, BusinessRepository businessDao, LocationRepository locationDao, TicketRepository ticketDao, PasswordEncoder passwordEncoder) {
         this.userDao = userDao;
         this.businessDao = businessDao;
         this.locationDao = locationDao;
         this.ticketDao = ticketDao;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/")
     public String index(Model model) {
         return "index";
+    }
+
+    @GetMapping("/home")
+    public String home(Model model){
+        return "home";
     }
 
     @GetMapping("/login")
@@ -36,6 +49,21 @@ public class UserController {
 
     @PostMapping("/login")
     public String logUser() {
-        return "redirect:/main";
+        return "redirect:/home";
+    }
+
+    @GetMapping("/register")
+    public String registerUserForm(Model model) {
+        model.addAttribute("user", new User());
+        return "sign-up";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user, Model model) {
+        model.addAttribute("user", new User());
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userDao.save(user);
+        return "redirect:/login";
     }
 }
