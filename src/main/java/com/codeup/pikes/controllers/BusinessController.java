02970ebb5f70f1cde9c2cb97189fdbab4e4f6a9a
Cody Hodges <hodges.cody00@gmail.com>
2022-06-05 13:18:10 -0400

    Businesses

--- src/main/java/com/codeup/pikes/controllers/BusinessController.java
+++ src/main/java/com/codeup/pikes/controllers/BusinessController.java
@@ -4,7 +4,12 @@ package com.codeup.pikes.controllers;
 import com.codeup.pikes.repositories.BusinessRepository;
 import com.codeup.pikes.repositories.LocationRepository;
 import com.codeup.pikes.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BusinessController {
@@ -18,4 +23,17 @@ public class BusinessController {
         this.userDao = userDao;
     }
 
+    @GetMapping("/contactList")
+    public String contact(Model model){
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        model.addAttribute("businesses", businessDao.findAll());
+        return "/contactList";
+    }
+
+    @PostMapping("/search")
+    public String searchIndex(Model model, @RequestParam(name = "search") String search) {
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        model.addAttribute("businesses", businessDao.searchBusinesses(search));
+        return "/contactList";
+    }
 }
--- src/main/java/com/codeup/pikes/repositories/BusinessRepository.java
+++ src/main/java/com/codeup/pikes/repositories/BusinessRepository.java
@@ -2,7 +2,17 @@ package com.codeup.pikes.repositories;
 
 import com.codeup.pikes.models.Business;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BusinessRepository extends JpaRepository<Business, Long> {
     Business findByName(String name);
+
+    @Query(value = "SELECT * FROM businesses ORDER BY name ASC", nativeQuery = true)
+    List<Business> getBusinessesAlphabetical();
+
+    @Query(value = "SELECT DISTINCT t.id, t.name FROM businesses t WHERE t.name LIKE CONCAT('%', ?1, '%')", nativeQuery = true)
+    List<Business> searchBusinesses(String search);
+
 }
