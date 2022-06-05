59142081693226549e3cbf94b8f5e51fd3430ce5
Cody Hodges <hodges.cody00@gmail.com>
2022-06-05 13:16:23 -0400

    Ticket Controller

--- src/main/java/com/codeup/pikes/controllers/TicketController.java
+++ src/main/java/com/codeup/pikes/controllers/TicketController.java
@@ -4,7 +4,9 @@ import com.codeup.pikes.repositories.BusinessRepository;
 import com.codeup.pikes.repositories.LocationRepository;
 import com.codeup.pikes.repositories.TicketRepository;
 import com.codeup.pikes.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -22,47 +24,56 @@ public class TicketController {
     }
 
     @GetMapping("/ticket1")
-    public String ticket1(){
+    public String ticket1(Model model){
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         return "ticket/ticket1";
     }
 
     @GetMapping("/ticket2")
-    public String ticket2(){
+    public String ticket2(Model model){
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         return "ticket/ticket2";
     }
 
     @GetMapping("/ticket3")
-    public String ticket3(){
+    public String ticket3(Model model){
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         return "ticket/ticket3";
     }
 
     @GetMapping("/ticket4")
-    public String ticket4(){
+    public String ticket4(Model model){
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         return "ticket/ticket4";
     }
 
     @GetMapping("/ticket5")
-    public String ticket5(){
+    public String ticket5(Model model){
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         return "ticket/ticket5";
     }
 
     @GetMapping("/ticket6")
-    public String ticket6(){
+    public String ticket6(Model model){
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         return "ticket/ticket6";
     }
 
     @GetMapping("/ticket7")
-    public String ticket7(){
+    public String ticket7(Model model){
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         return "ticket/ticket7";
     }
 
     @GetMapping("/ticket8")
-    public String ticket8(){
+    public String ticket8(Model model){
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         return "ticket/ticket8";
     }
 
     @GetMapping("/ticket9")
-    public String ticket9(){
+    public String ticket9(Model model){
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         return "ticket/ticket9";
     }
 
