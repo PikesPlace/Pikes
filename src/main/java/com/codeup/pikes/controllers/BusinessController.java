97d8c7d929f48a726e8ee5342917dc910f1e3443
Cody Hodges <hodges.cody00@gmail.com>
2022-06-06 17:25:02 -0400

    Business locations page start

--- src/main/java/com/codeup/pikes/controllers/BusinessController.java
+++ src/main/java/com/codeup/pikes/controllers/BusinessController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -35,5 +36,11 @@ public class BusinessController {
         model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         model.addAttribute("businesses", businessDao.searchBusinesses(search));
         return "/contactList";
+    }
+
+    @GetMapping("/bizLocations/{id}")
+    public String bizLocations(@PathVariable Long id, Model model){
+//        model.addAttribute("locations", businessDao.findLocations(id));
+        return "/bizLocations";
     }
 }
