350c6ba406b7b61b1c2310e3ab6b47d09c67ae20
Cody Hodges <hodges.cody00@gmail.com>
2022-06-07 11:47:01 -0400

    Start Ticket Types

---
+++ src/main/java/com/codeup/pikes/models/JobType.java
@@ -0,0 +1,43 @@
+package com.codeup.pikes.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "JobTypes")
+public class JobType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String name;
+
+    @ManyToMany(mappedBy = "jobTypes")
+    private List<Ticket> ticketJobType;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Ticket> getTicketJobType() {
+        return ticketJobType;
+    }
+
+    public void setTicketJobType(List<Ticket> ticketJobType) {
+        this.ticketJobType = ticketJobType;
+    }
+}
--- src/main/java/com/codeup/pikes/models/Ticket.java
+++ src/main/java/com/codeup/pikes/models/Ticket.java
@@ -1,6 +1,8 @@
 package com.codeup.pikes.models;
 
 import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "tickets")
@@ -8,6 +10,32 @@ public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column
+    private Long ticketNum;
+
+    @OneToOne
+    private Location location;
+
+    @OneToOne
+    private Business business;
+
+    @Column
+    private Date date;
+
+    @Column
+    private Date dateOrdered;
+
+    @Column
+    private Date dateScheduled;
+
+    @Column
+    private String phoneNum;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "ticket_types", joinColumns = {@JoinColumn(name = "ticket_id")}, inverseJoinColumns = {@JoinColumn(name = "jobtype_id")})
+    private List<JobType> jobTypes;
+
 
     public Ticket(){}
 
@@ -17,5 +45,69 @@ public class Ticket {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public List<JobType> getJobTypes() {
+        return jobTypes;
+    }
+
+    public void setJobTypes(List<JobType> jobTypes) {
+        this.jobTypes = jobTypes;
+    }
+
+    public Long getTicketNum() {
+        return ticketNum;
+    }
+
+    public void setTicketNum(Long ticketNum) {
+        this.ticketNum = ticketNum;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Business getBusiness() {
+        return business;
+    }
+
+    public void setBusiness(Business business) {
+        this.business = business;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Date getDateOrdered() {
+        return dateOrdered;
+    }
+
+    public void setDateOrdered(Date dateOrdered) {
+        this.dateOrdered = dateOrdered;
+    }
+
+    public Date getDateScheduled() {
+        return dateScheduled;
+    }
+
+    public void setDateScheduled(Date dateScheduled) {
+        this.dateScheduled = dateScheduled;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
     }
 }
