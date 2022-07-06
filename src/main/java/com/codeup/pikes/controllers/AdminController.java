4b6514355f2feb38c2d9801900b1244e53e6a032
Cody Hodges <hodges.cody00@gmail.com>
2022-07-05 21:18:05 -0400

    Admin add page

--- src/main/java/com/codeup/pikes/controllers/AdminController.java
+++ src/main/java/com/codeup/pikes/controllers/AdminController.java
@@ -1,5 +1,8 @@
 package com.codeup.pikes.controllers;
 
+import com.codeup.pikes.models.Business;
+import com.codeup.pikes.models.Location;
+import com.codeup.pikes.models.User;
 import com.codeup.pikes.repositories.BusinessRepository;
 import com.codeup.pikes.repositories.LocationRepository;
 import com.codeup.pikes.repositories.TicketRepository;
@@ -7,6 +10,9 @@ import com.codeup.pikes.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdminController {
@@ -32,4 +38,24 @@ public class AdminController {
         model.addAttribute("tickets", ticketDao.findAll());
         return "admin";
     }
+
+    @GetMapping("/adminAdd")
+    public String adminAdd(Model model){
+        model.addAttribute("newUser", new User());
+        model.addAttribute("newBusiness", new Business());
+        model.addAttribute("businesses", businessDao.findAll());
+        model.addAttribute("newLocation", new Location());
+        return "adminAdd";
+    }
+
+    @PostMapping("/adminAddUser")
+    public String adminAddUser(@ModelAttribute User user, @RequestParam ("isAdmin") Boolean isAdmin, @RequestParam ("email") String email, @RequestParam ("pass") String pass){
+        user.setPassword(pass);
+        user.setEmail(email);
+        user.setAdmin(isAdmin);
+        userDao.save(user);
+        return "admin";
+    }
+
+
 }
