37ebf2066f965775a596b6ca631d9949b6487ff5
Cody Hodges <hodges.cody00@gmail.com>
2022-07-07 19:23:38 -0400

    Query to find locations by Business id

--- src/main/java/com/codeup/pikes/repositories/LocationRepository.java
+++ src/main/java/com/codeup/pikes/repositories/LocationRepository.java
@@ -1,8 +1,16 @@
 package com.codeup.pikes.repositories;
 
 import com.codeup.pikes.models.Location;
+import org.hibernate.sql.Select;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
     Location findByName(String name);
+
+    @Query(value = "SELECT * FROM locations WHERE business_id LIKE CONCAT('%', ?1, '%')", nativeQuery = true)
+    List<Location> findLocations(Long id);
+
 }
