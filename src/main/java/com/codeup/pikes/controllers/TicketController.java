bb12ce3f403821da89b92e42d0c2bd0e361309a0
Cody Hodges <hodges.cody00@gmail.com>
2022-07-08 17:41:56 -0400

    Cancel Ticket

--- src/main/java/com/codeup/pikes/controllers/TicketController.java
+++ src/main/java/com/codeup/pikes/controllers/TicketController.java
@@ -8,10 +8,7 @@ import com.codeup.pikes.repositories.*;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TicketController {
@@ -29,6 +26,14 @@ public class TicketController {
         this.jobTypeDao = jobTypeDao;
     }
 
+//    Cancel ticket
+    @PostMapping("/cancelTicket")
+    public String cancelTicket(@RequestParam("cancelId") Long id){
+        ticketDao.deleteById(id);
+        return "/home";
+    }
+
+//    Page 1
     @GetMapping("/ticket1")
     public String ticket1(Model model){
         model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
@@ -48,10 +53,60 @@ public class TicketController {
         return "redirect:/ticket2/" + ticket.getId();
     }
 
+//    Page 2
     @GetMapping("/ticket2/{id}")
-    public String ticket2(Model model){
-
+    public String ticket2(Model model, @PathVariable Long id){
+        model.addAttribute("ticket", ticketDao.getById(id));
         return "ticket/ticket2";
+    }
+
+//    Page 3
+    @GetMapping("/ticket3/{id}")
+    public String ticket3(Model model, @PathVariable Long id){
+        model.addAttribute("ticket", ticketDao.getById(id));
+        return "ticket/ticket3";
+    }
+
+//    Page 4
+    @GetMapping("/ticket4/{id}")
+    public String ticket4(Model model, @PathVariable Long id){
+        model.addAttribute("ticket", ticketDao.getById(id));
+        return "ticket/ticket4";
+    }
+
+//    Page 5
+    @GetMapping("/ticket5/{id}")
+    public String ticket5(Model model, @PathVariable Long id){
+        model.addAttribute("ticket", ticketDao.getById(id));
+        return "ticket/ticket5";
+    }
+
+//    Page 6
+    @GetMapping("/ticket6/{id}")
+    public String ticket6(Model model, @PathVariable Long id){
+        model.addAttribute("ticket", ticketDao.getById(id));
+        return "ticket/ticket6";
+    }
+
+//    Page 7
+    @GetMapping("/ticket7/{id}")
+    public String ticket7(Model model, @PathVariable Long id){
+        model.addAttribute("ticket", ticketDao.getById(id));
+        return "ticket/ticket7";
+    }
+
+//    Page 8
+    @GetMapping("/ticket8/{id}")
+    public String ticket8(Model model, @PathVariable Long id){
+        model.addAttribute("ticket", ticketDao.getById(id));
+        return "ticket/ticket8";
+    }
+
+//    Page 9
+    @GetMapping("/ticket9/{id}")
+    public String ticket9(Model model, @PathVariable Long id){
+        model.addAttribute("ticket", ticketDao.getById(id));
+        return "ticket/ticket9";
     }
 
 
