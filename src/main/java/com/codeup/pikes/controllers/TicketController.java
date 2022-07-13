a2fea40e5bf41e1563d429af24cd71317a3c431f
Cody Hodges <hodges.cody00@gmail.com>
2022-07-12 22:12:37 -0400

    Add PostMapping for all Ticket pages

--- src/main/java/com/codeup/pikes/controllers/TicketController.java
+++ src/main/java/com/codeup/pikes/controllers/TicketController.java
@@ -57,64 +57,104 @@ public class TicketController {
     @GetMapping("/ticket2/{id}")
     public String ticket2(Model model, @PathVariable Long id) {
         model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        model.addAttribute("ticket", ticketDao.getById(id));
+        model.addAttribute("ticketObj", ticketDao.getById(id));
         return "ticket/ticket2";
+    }
+
+    @PostMapping("/ticket2End")
+    public String ticket2End(@ModelAttribute Ticket ticket){
+        return "redirect:/ticket/3/" + ticket.getId();
     }
 
     //    Page 3
     @GetMapping("/ticket3/{id}")
     public String ticket3(Model model, @PathVariable Long id) {
         model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        model.addAttribute("ticket", ticketDao.getById(id));
+        model.addAttribute("ticketObj", ticketDao.getById(id));
         return "ticket/ticket3";
+    }
+
+    @PostMapping("/ticket3End")
+    public String ticket3End(@ModelAttribute Ticket ticket){
+        return "redirect:/ticket/4/" + ticket.getId();
     }
 
     //    Page 4
     @GetMapping("/ticket4/{id}")
     public String ticket4(Model model, @PathVariable Long id) {
         model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        model.addAttribute("ticket", ticketDao.getById(id));
+        model.addAttribute("ticketObj", ticketDao.getById(id));
         return "ticket/ticket4";
+    }
+
+    @PostMapping("/ticket4End")
+    public String ticket4End(@ModelAttribute Ticket ticket){
+        return "redirect:/ticket/5/" + ticket.getId();
     }
 
     //    Page 5
     @GetMapping("/ticket5/{id}")
     public String ticket5(Model model, @PathVariable Long id) {
         model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        model.addAttribute("ticket", ticketDao.getById(id));
+        model.addAttribute("ticketObj", ticketDao.getById(id));
         return "ticket/ticket5";
+    }
+
+    @PostMapping("/ticket5End")
+    public String ticket5End(@ModelAttribute Ticket ticket){
+        return "redirect:/ticket/6/" + ticket.getId();
     }
 
     //    Page 6
     @GetMapping("/ticket6/{id}")
     public String ticket6(Model model, @PathVariable Long id) {
         model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        model.addAttribute("ticket", ticketDao.getById(id));
+        model.addAttribute("ticketObj", ticketDao.getById(id));
         return "ticket/ticket6";
+    }
+
+    @PostMapping("/ticket6End")
+    public String ticket6End(@ModelAttribute Ticket ticket){
+        return "redirect:/ticket/7/" + ticket.getId();
     }
 
     //    Page 7
     @GetMapping("/ticket7/{id}")
     public String ticket7(Model model, @PathVariable Long id) {
         model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        model.addAttribute("ticket", ticketDao.getById(id));
+        model.addAttribute("ticketObj", ticketDao.getById(id));
         return "ticket/ticket7";
+    }
+
+    @PostMapping("/ticket7End")
+    public String ticket7End(@ModelAttribute Ticket ticket){
+        return "redirect:/ticket/8/" + ticket.getId();
     }
 
     //    Page 8
     @GetMapping("/ticket8/{id}")
     public String ticket8(Model model, @PathVariable Long id) {
         model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        model.addAttribute("ticket", ticketDao.getById(id));
+        model.addAttribute("ticketObj", ticketDao.getById(id));
         return "ticket/ticket8";
+    }
+
+    @PostMapping("/ticket8End")
+    public String ticket8End(@ModelAttribute Ticket ticket){
+        return "redirect:/ticket/9/" + ticket.getId();
     }
 
     //    Page 9
     @GetMapping("/ticket9/{id}")
     public String ticket9(Model model, @PathVariable Long id) {
         model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-        model.addAttribute("ticket", ticketDao.getById(id));
+        model.addAttribute("ticketObj", ticketDao.getById(id));
         return "ticket/ticket9";
+    }
+
+    @PostMapping("/ticket9End")
+    public String ticket9End(@ModelAttribute Ticket ticket){
+        return "redirect:/home";
     }
 
 
