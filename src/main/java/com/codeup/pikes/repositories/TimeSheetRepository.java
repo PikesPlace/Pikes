6c017f80c604b029b2a77868ab6a5b41a267d3db
Cody Hodges <hodges.cody00@gmail.com>
2022-07-19 00:58:32 -0400

    Time Sheet Query for clock out form

---
+++ src/main/java/com/codeup/pikes/repositories/TimeSheetRepository.java
@@ -0,0 +1,14 @@
+package com.codeup.pikes.repositories;
+
+import com.codeup.pikes.models.TimeSheet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
+import java.util.List;
+
+public interface TimeSheetRepository extends JpaRepository<TimeSheet, Long> {
+
+    @Query(value = "SELECT * FROM time_sheets WHERE time_sheets.user_id = ? && time_sheets.this_date LIKE CONCAT('%', ?1, '%')", nativeQuery = true)
+    TimeSheet timeSheetFind(Long id, Date currentDate);
+}
