583f44ad45e7e9ae7a17eed92a301c5e3a90ffbd
Cody Hodges <hodges.cody00@gmail.com>
2022-07-19 00:58:50 -0400

    Seeder

--- seeder.sql
+++ seeder.sql
@@ -16,4 +16,3 @@ insert into job_types (name) values ('Normal');
 
 insert into tickets (date, date_ordered, date_scheduled, phone_num, ticket_num, business_id, location_id) VALUES ('04/02/20', '04/01/20', '04/03/20', 4567654645, 1, 1, 1);
 
-# insert into ticket_type (ticket_id, job_type_id) VALUES (1, 1);
