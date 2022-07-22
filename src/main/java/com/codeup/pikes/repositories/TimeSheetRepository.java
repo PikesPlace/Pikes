94a9b6112128bcb2c258e2aad44634953b614915
Cody Hodges <hodges.cody00@gmail.com>
2022-07-21 23:08:33 -0400

    Work on ClockOut function

--- src/main/java/com/codeup/pikes/repositories/TimeSheetRepository.java
+++ src/main/java/com/codeup/pikes/repositories/TimeSheetRepository.java
@@ -3,12 +3,13 @@ package com.codeup.pikes.repositories;
 import com.codeup.pikes.models.TimeSheet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
 
 public interface TimeSheetRepository extends JpaRepository<TimeSheet, Long> {
 
-    @Query(value = "SELECT * FROM time_sheets WHERE time_sheets.user_id = ? && time_sheets.this_date LIKE CONCAT('%', ?, '%')", nativeQuery = true)
+    @Query(value = "SELECT time_out FROM time_sheets u WHERE u.user_id = ?1 && u.this_date LIKE ?2 LIMIT 1" , nativeQuery = true)
     TimeSheet timeSheetFind(Long id, Date currentDate);
 }
