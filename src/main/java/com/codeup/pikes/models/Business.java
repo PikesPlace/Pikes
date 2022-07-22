831b9cab4cbccabad03973986759e65632322ff7
Cody Hodges <hodges.cody00@gmail.com>
2022-07-22 00:29:49 -0400

    Business model update

--- src/main/java/com/codeup/pikes/models/Business.java
+++ src/main/java/com/codeup/pikes/models/Business.java
@@ -13,14 +13,30 @@ public class Business {
     @Column(nullable = false)
     private String name;
 
+    @Column
+    private String street;
+
+    @Column
+    private String city;
+
+    @Column
+    private String state;
+
+    @Column
+    private String zip;
+
     @OneToMany(mappedBy = "business")
     private List<Location> locations;
 
     public Business(){}
 
-    public Business(long id, String name, List<Location> locations){
+    public Business(long id, String name, List<Location> locations, String street, String city, String state, String zip){
         this.id = id;
         this.name = name;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
         this.locations = locations;
     }
 
@@ -47,5 +63,37 @@ public class Business {
 
     public void setLocations(List<Location> locations) {
         this.locations = locations;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
     }
 }
