491323454c3aa6074eb6a3b619c0e2a1a3fd6370
Cody Hodges <hodges.cody00@gmail.com>
2022-07-24 18:08:58 -0400

    Ticket search query

--- src/main/java/com/codeup/pikes/repositories/TicketRepository.java
+++ src/main/java/com/codeup/pikes/repositories/TicketRepository.java
@@ -1,8 +1,15 @@
 package com.codeup.pikes.repositories;
 
+import com.codeup.pikes.models.Business;
 import com.codeup.pikes.models.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    @Query(value = "SELECT * FROM tickets t WHERE t.ticket_num LIKE CONCAT('%', ?1, '%')", nativeQuery = true)
+    List<Ticket> searchTickets(Long search);
 
 }
